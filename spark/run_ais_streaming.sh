915fd6f137db302d21a4c25d37e4dd18965ec337
Doha-Ahmed-E <dohaamed22@gmail.com>
2026-08-19 16:29:48 +0300

    fix: file name typo

--- spark/run_ais_streaming.sh
+++ spark/run_ais_streaming.sh
@@ -5,4 +5,4 @@ exec /opt/spark/bin/spark-submit \
     --conf spark.cores.max=4 \
     --conf spark.jars.ivy=/tmp/.ivy2 \
     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.6 \
-    /opt/spark-apps/jobs/ais_streaming.py.py
+    /opt/spark-apps/jobs/ais_streaming.py
